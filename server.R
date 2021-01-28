b8fff4a788de2d222080bddc0fbc3866ab862000
jayruffell <ruffell.jay@gmail.com>
2021-01-29 10:40:13 +1300

    code now working same as original google sheets code

--- server.R
+++ server.R
@@ -1,23 +1,44 @@
-#  Define server logic to summarize and view selected dataset ----
+
+library(scales)
+
 server <- function(input, output) {
   
-  # Return the requested dataset ----
-  datasetInput <- reactive({
-    switch(input$dataset,
-           "rock" = rock,
-           "pressure" = pressure,
-           "cars" = cars)
+  #________________________________________________________________________
+  
+  # Prelim calculations ----
+  #________________________________________________________________________
+  
+  # specify savings per mo until retiring # ASSUMES PAYING MORTGAGE RIGHT UP TO RETIREMENT AGE
+  savings_per_mo_pre_ret = 
+    reactive({input$income - input$mortgage - input$expenses})
+
+  # Specify expenses per mo in retirement. THIS ASSUMES NO MORTGAGE BY RETIREMENT AGE, ALSO PENSION IMMEDIATELY. BUT THIS IS INCORRECT IF WE RETIRE EARLY ENOUGH!
+  expenses_per_mo_post_ret =
+    reactive({input$expenses*(input$retirement_spend_rate/100) - input$pension})
+
+  # specify tot assets required to provide retirement expenses perpetually
+  assets_target = reactive({
+    (1/(input$withdrawal_rate/100)) * 12 * expenses_per_mo_post_ret()
+  })
+
+  # specify total amount to save to hit assets target
+  savings_target =
+    reactive({assets_target() - input$kiwisaver - input$house_downsizing_payoff})
+
+  # years to achieve savings target
+  years = reactive({
+    savings_target() / (savings_per_mo_pre_ret() * 12)
   })
   
-  # Generate a summary of the dataset ----
-  output$summary <- renderPrint({
-    dataset <- datasetInput()
-    summary(dataset)
-  })
+  #________________________________________________________________________
   
-  # Show the first "n" observations ----
-  output$view <- renderTable({
-    head(datasetInput(), n = input$obs)
-  })
+  # Structure outputs ----
+  #________________________________________________________________________
+  
+  output$sav_per_mo = renderText(comma(savings_per_mo_pre_ret()))
+  output$exp_per_mo = renderText(comma(expenses_per_mo_post_ret()))
+  output$ass_reqd = renderText(comma(assets_target()))
+  output$sav_reqd = renderText(comma(savings_target()))
+  output$years = renderText(comma(years()))
   
 }
--- ui.R
+++ ui.R
@@ -1,35 +1,82 @@
+# TO DO 
+# add in annual salary increase
+# loan amont paid off, possibly converting to % value of current house after additional input for house inflation. amortisation rate could come from here - mortgage: at a given no. of periods, interest rate, and principal value, how much will be left on loan? possibly here for rearranging?  n=−log(1−rPVPMT)log(1+r) from https://math.stackexchange.com/questions/2265192/solve-for-n-for-loan-amortization?
+# nice rounding
+# allow for reverse mortgage
+# factor in Deflation of savings per Ec Explained
+
 # Define UI for dataset viewer app ----
 ui <- fluidPage(
   
   # App title ----
-  titlePanel("Shiny Text"),
+  titlePanel("Jemily on FIRE"),
   
   # Sidebar layout with a input and output definitions ----
   sidebarLayout(
     
-    # Sidebar panel for inputs ----
+    #________________________________________________________________________
+    
+    # Inputs ----
+    #________________________________________________________________________
+
     sidebarPanel(
       
-      # Input: Selector for choosing dataset ----
-      selectInput(inputId = "dataset",
-                  label = "Choose a dataset:",
-                  choices = c("rock", "pressure", "cars")),
+      #++++++++++++++
+      # initial values
+      #++++++++++++++
       
-      # Input: Numeric entry for number of obs to view ----
-      numericInput(inputId = "obs",
-                   label = "Number of observations to view:",
-                   value = 10)
+      numericInput(inputId = "mortgage",
+                   label = "Monthly expenses - mortgage:",
+                   value = 2900),
+      numericInput(inputId = "expenses",
+                   label = "Monthly expenses - other:",
+                   value = 7000),
+      numericInput(inputId = "income",
+                   label = "Monthly after-tax income:",
+                   value = 12000),
+      numericInput(inputId = "withdrawal_rate",
+                   label = "Annual withdrawal rate (%):",
+                   value = 3), # 4% often advocated for US, 2.5-3% for UK. See  https://moneyed.co.uk/blog/intro_to_fire. Also, from https://en.wikipedia.org/wiki/Trinity_study see criticicms, e.g.: Laurence Kotlikoff, advocate of the consumption smoothing theory of retirement planning, is even less kind to the 4% rule, saying that it "has no connection to economics.... economic theory says you need to adjust your spending based on the portfolio of assets you're holding. If you invest aggressively, you need to spend defensively. Notice that the 4 percent rule has no connection to the other rule—to target 85 percent of your preretirement income. The whole thing is made up out of the blue."[7]
+      numericInput(inputId = "retirement_spend_rate",
+                   label = "Retirement spend rate (%):",
+                   value = 85),
+      numericInput(inputId = "pension",
+                   label = "Monthly after-tax pension:",
+                   value = 2600),
+      
+      #++++++++++++++
+      # current assets
+      #++++++++++++++
+      
+      numericInput(inputId = "kiwisaver",
+                   label = "Current kiwisaver amount:",
+                   value = 30000),
+      numericInput(inputId = "house_downsizing_payoff",
+                   label = "downsizing:",
+                   value = 0)
     ),
     
-    # Main panel for displaying outputs ----
+    #________________________________________________________________________
+    
+    # Outputs ----
+    #________________________________________________________________________
+    
     mainPanel(
       
-      # Output: Verbatim text for data summary ----
-      verbatimTextOutput("summary"),
-      
-      # Output: HTML table with requested number of observations ----
-      tableOutput("view")
-      
+      hr('Savings per month until retirement:'), 
+      textOutput('sav_per_mo', inline=T),
+      br(),
+      hr('Expenses per month in retirement:'), 
+      textOutput('exp_per_mo', inline=T),
+      br(),
+      hr('Assets required to provide retirement expenses perpetually:'),
+      textOutput('ass_reqd', inline=T),
+      br(),
+      hr('Total amount to save:'),
+      textOutput('sav_reqd', inline=T),
+      br(),
+      hr('Years to achieve'),
+      textOutput('years', inline=T),
     )
   )
 )
